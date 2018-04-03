04e09bb4b85cb1381cb6d3366817ec8b08c1bc79
jagans94 <jagans94@gmail.com>
2018-04-03 23:25:45 +0530

    fixed bugs

--- competitions/scripts/my_utils.py
+++ competitions/scripts/my_utils.py
@@ -9,37 +9,38 @@ from sklearn.model_selection import train_test_split
 
 # Methods:
 
-def bin_dataset(directory, mapping, labels = [], validation_split = 0.0, random_state = 0):
-    """Bins the dataset (given in the mapping representation) into respective classes.
-       Use this to bin the entire dataset, or just a subset of it. Complements the 'ImageDataGenerator' class in Keras.
-       In case of train/val split, the resulting mappings are stored into a '.csv' file in respective 'train' and 'val' folders.
+def bin_dataset(base_dir, sub_dir, mapping, validation_split = 0.0, labels = [], random_state = 0):
+    """Bins the dataset into respective classes and returns a path to the 'binned' directory.
+       Complements the 'ImageDataGenerator' class in Keras.
+       The train/val split mappings are stored into '.csv' files.
 
     # Arguments
-        directory: Source directory.
-        mapping: Numpy array of shape (N,2). First column should contain filenames/id and the second column 
-                 corresponding ground truth labels. 
-        [labels]: Class labels. Needs a minimum of 2 classes. If 'empty', inferred from the mapping. 
-                  You can even pass a subset of labels, if required.
-        [validation_split]: A float in the half-interval range [0,1). Used to split the 'dataset' into training and validation sets.
+        base_dir: Base directory
+        sub_dir: Name of sub-directory, you want 'binned'.
+        mapping: Numpy array of shape (N,2). First column -> filenames/id; Second column  -> labels. 
+        [labels]: Class labels. Minimum of 2 needed. If 'empty', inferred from the mapping. 
+        [validation_split]: Used to split the 'dataset' into training and validation sets.
         [random_state] : Random seed for train/val split.
+
+    # Returns
+        path -> base_dir/binned_dir
     
     """
     if labels == []: labels = np.unique(mapping[:,1])
-    # no. of files in directory
-    tnof = len([fname for fname in os.listdir(directory) if os.path.isfile(os.path.join(directory, fname))])
+    _dir = os.path.join(base_dir, sub_dir)
+    _has_files = ([os.path.isfile(os.path.join(_dir, fname)) for fname in os.listdir(_dir)]).any()
 
     # Checks
-    assert len(dict(mapping)) == len(mapping), "The mapping is not hashable!!"
-    assert os.path.isdir(directory), "The directory doesnot exist!!"
-    assert mapping.shape == mapping.reshape(-1,2).shape, "The mapping provided is incorrect. Check the doc string for more info."
-    assert labels.size > 1, "Too few labels!!"
-    assert validation_split in np.arange(0.0,1.0,1e-3), "The value provided for 'validation_split' arg is invalid. Check the doc string for more info."
-    assert tnof > 0, "There are no files in the directory."
+    assert os.path.isdir(_dir), "Error: The directory doesn't exist."
+    assert _has_files, "Error: No files found."
+    assert mapping.shape == mapping.reshape(-1,2).shape, "Error: The mapping can't be resolved."
+    assert labels.size > 1, "Error: The size of 'labels' needs to be strictly > 1."
+    assert validation_split in np.arange(0.0,1.0,1e-3), "Error: Invalid argument value: 'validation_split'"
     print('Tests passed.')
     print('Total no. of files: ', )
     print('Selected no. of files: ', len(mapping))
 
-    print('\nThis might take a while...')
+    print('\nThis might take a while...', end = '. ')
     start = time.time()
 
     # Helper functions
@@ -49,26 +50,28 @@ def bin_dataset(directory, mapping, labels = [], validation_split = 0.0, random_
             if not os.path.exists(path):
                 os.makedirs(path)
     
-    def move_files_into_labels_folders(src_dir, dst_dir, mapping):
+    def copy_files_into_labels_folders(src_dir, dst_dir, mapping):
         for fname, label in mapping.items():
             fname += '.jpg'
             src = os.path.join(src_dir,fname)
             dst = os.path.join(dst_dir, os.path.join(label,fname))
-            if os.path.isfile(src): shutil.move(src,dst)
+            if os.path.isfile(src): shutil.copy(src,dst)
+    
+    # Directory paths
+    bin_dir = os.path.join(base_dir, 'bin')
+    train_path = os.path.join(bin_dir, 'train')
+    val_path = os.path.join(bin_dir, 'val')
 
     # Split and bin the files into training and validation sets.
     if validation_split > 0.0:
         # Computing the split
-        train_path = os.path.join(directory,'train')
-        val_path = os.path.join(directory, 'val')
-
         X, y = mapping[:,0], mapping[:,1]
-        X_train, X_val, y_train, y_val = train_test_split(  
-                                                    X, y,
-                                                    test_size = validation_split, 
-                                                    random_state=random_state, 
-                                                    stratify = y,
-                                                    )
+        X_train, X_val, y_train, y_val 
+                    = train_test_split( X, y,
+                                        test_size = validation_split, 
+                                        random_state=random_state, 
+                                        stratify = y,
+                                        )
         # Need to reshape the numpy arrays to use them
         X_train = X_train.reshape(-1,1)
         y_train = y_train.reshape(-1,1)
@@ -78,27 +81,27 @@ def bin_dataset(directory, mapping, labels = [], validation_split = 0.0, random_
         train_mapping = np.concatenate([X_train,y_train], axis = 1)
         val_mapping = np.concatenate((X_val,y_val), axis = 1)
 
-        # Saving the mapping to '.csv' files
+        # Saving mappings to '.csv' files
         data_tmap = pd.DataFrame(train_mapping, column = ['id','label'])
         data_vmap = pd.DataFrame(val_mapping, column = ['id','label'])
-        data_tmap.to_csv(train_path)
-        data_vmap.to_csv(val_path)
+        data_tmap.to_csv(os.path.join(bin_dir), 'train_map.csv')
+        data_vmap.to_csv(os.path.join(bin_dir), 'val_map.csv')
         print("No. of train samples:" ,train_mapping.shape[0])
         print("No. of validation samples:" ,val_mapping.shape[0])
 
         # Binning the files
         create_labels_folder(train_path, labels)
         create_labels_folder(val_path, labels)
-        move_files_into_labels_folders(directory, train_path, dict(train_mapping))
-        move_files_into_labels_folders(directory, val_path, dict(val_mapping))
+        copy_files_into_labels_folders(_dir, train_path, dict(train_mapping))
+        copy_files_into_labels_folders(_dir, val_path, dict(val_mapping))
     
     else:
         # Binning the files
-        create_labels_folder(directory, labels)
-        move_files_into_labels_folders(directory, directory, dict(mapping))
+        create_labels_folder(train_path, labels)
+        copy_files_into_labels_folders(_dir, train_path, dict(mapping))
 
     end = time.time()
-    print('Finished. Time taken: %.2fs.' %(end - start))
+    print('Done. Time taken: %.2fs.' %(end - start))
 
 
 def unpack_dataset(src_dir, dest_dir):
